a1db2a2c18b183d2768acc005bc9734295d6fbd7
PedroH-Kora <pedro.magalhaes@korasaude.com.br>
2024-08-19 17:45:40 -0300

    refactor: Rota tickets preview

--- app/controllers/tickets_controller.py
+++ app/controllers/tickets_controller.py
@@ -98,3 +98,49 @@ def get_minha_equipe():
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@tickets_blueprint.route('/tickets-preview', methods=['GET'])
+def get_tickets_preview():
+    cod_fluxo = request.args.get('id')
+    
+    page = request.args.get('page', 1, type=int)
+    per_page = request.args.get('per_page', 10, type=int)
+
+    try:
+        query = TbTickets.query
+
+        if cod_fluxo:
+            query = query.filter(TbTickets.cod_fluxo.like(f'%{cod_fluxo}%'))
+        
+        paginated_tickets = query.paginate(page=page, per_page=per_page, error_out=False)
+
+        results = [
+            {
+                "id": ticket.id,
+                "cod_fluxo": ticket.cod_fluxo,
+                "abertura": ticket.abertura,
+                "status": ticket.status,
+                "sla_util": ticket.st_sla,
+                "data_limite": ticket.data_limite,
+                "grupo": ticket.grupo,
+                "nome": ticket.nome,
+                "area_negocio": ticket.area_negocio,
+                "hub": ticket.hub,
+                "unidade": ticket.unidade,
+                "categoria": ticket.categoria,
+                "subcategoria": ticket.subcategoria,
+                "assunto": ticket.assunto 
+            }
+            for ticket in paginated_tickets.items
+        ]
+
+        return jsonify({
+            "page": paginated_tickets.page,
+            "pages": paginated_tickets.pages,
+            "total_items": paginated_tickets.total,
+            "items_per_page": paginated_tickets.per_page,
+            "tickets": results
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
