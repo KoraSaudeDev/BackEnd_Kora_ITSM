8838b798de1de1bb3127990db33e2f2b15fa432c
PedroH-Kora <pedro.magalhaes@korasaude.com.br>
2024-10-08 11:30:44 -0300

    feat: bloqueando requisições fora vpn

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_cors import CORS
 
 db = SQLAlchemy()
 mail = Mail()
+
+ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://helper.korasaude.com.br",
+    "https://qashelper.korasaude.com.br"
+]
+
+ALLOWED_VPN_IPS = ["192.168.80.0/24", "10.188.233.36", "127.0.0.1"]
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +21,7 @@ def create_app():
     db.init_app(app)
     mail.init_app(app)
     
-    CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": ALLOWED_ORIGINS}})
 
     @app.before_request
     def log_request_info():
@@ -21,7 +29,18 @@ def create_app():
         referer = request.headers.get('Referer', 'N/A')
         print(f"{request.path} - X-User-Email: {x_user_email} - Referer: {referer}")
 
+        origin = request.headers.get('Origin')
+        if origin not in ALLOWED_ORIGINS:
+            client_ip = request.remote_addr
+
+            if not any(ip_in_subnet(client_ip, subnet) for subnet in ALLOWED_VPN_IPS):
+                abort(403)
+
     from app.views.routes import main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
+
+def ip_in_subnet(ip, subnet):
+    from ipaddress import ip_address, ip_network
+    return ip_address(ip) in ip_network(subnet)
