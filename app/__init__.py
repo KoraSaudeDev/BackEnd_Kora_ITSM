9338f674265dc579ceb7a42ce119e66626d9dc8d
PedroH-Kora <pedro.magalhaes@korasaude.com.br>
2024-10-08 10:35:14 -0300

    testando bloqueio de rede

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_cors import CORS
@@ -13,43 +13,13 @@ def create_app():
     db.init_app(app)
     mail.init_app(app)
     
-    CORS(app, resources={r"/*": {"origins": [
-        "https://helper.korasaude.com.br",
-        "https://qashelper.korasaude.com.br",
-        "http://localhost:3000"
-    ]}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     @app.before_request
     def log_request_info():
         x_user_email = request.headers.get('X-User-Email', 'N/A')
         referer = request.headers.get('Referer', 'N/A')
         print(f"{request.path} - X-User-Email: {x_user_email} - Referer: {referer}")
-
-        allowed_origins = [
-            "https://helper.korasaude.com.br",
-            "https://qashelper.korasaude.com.br",
-            "http://localhost:3000"
-        ]
-
-        origin = request.headers.get('Origin')
-        user_agent = request.headers.get('User-Agent', '').lower()
-        
-        allowed_ip = '192.168.80.250'
-        
-        if request.headers.getlist("X-Forwarded-For"):
-            request_ip = request.headers.getlist("X-Forwarded-For")[0]
-        else:
-            request_ip = request.remote_addr
-        
-        print("REQUEST IP", request_ip, request.headers)
-
-        disallowed_agents = ['postman', 'insomnia', 'soapui']
-
-        if origin not in allowed_origins and request_ip != allowed_ip:
-            return jsonify({'error': 'Origin or IP not allowed'}), 403
-
-        if request_ip != allowed_ip and any(agent in user_agent for agent in disallowed_agents):
-            return jsonify({'error': 'Requests from this tool are not allowed from this IP'}), 403
 
     from app.views.routes import main_blueprint
     app.register_blueprint(main_blueprint)
